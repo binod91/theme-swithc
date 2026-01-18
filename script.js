bd831b0aea46979441bc7ad5f1ecccb86924f22b
Binod Pudasaini <binodpudasaini0@gmail.com>
2026-01-18 19:29:36 +1030

    changed in styles file and added a code to drop down

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const themes=[{
   message: "Hello Hello, This us Blue"
 }
 ]
+const themeSwitcherButton = document.querySelector('#theme-switcher-button');
+const themeDropDown = document.querySelector('#theme-dropdown');
+themeSwitcherButton.addEventListener('click', ()=>{
+    themeDropDown.hidden = !(themeDropDown.hidden)
+})
